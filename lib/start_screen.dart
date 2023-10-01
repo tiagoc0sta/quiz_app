ea8d43c0cebbc0d725d6fd9b3e7a7a42c9474e6a
Tiago Costa <tiagodpv@gmail.com>
2023-10-01 01:55:06 -0400

    start scren

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,8 +5,32 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('Start Screen'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
     );
   }
 }
